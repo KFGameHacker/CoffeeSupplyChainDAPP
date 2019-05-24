2cbbe6161e08d5f55ab4e5574876ef3ef9f817df
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-05-24 21:44:32 +0800

    test passed: retailerRole contract

---
+++ test/TestConsumerRole.js
@@ -0,0 +1,100 @@
+let FarmerRole = artifacts.require('FarmerRole');
+
+contract('FarmerRole',(accounts)=>{
+
+    let FarmerRoleManager;
+    const owner = accounts[0]
+    const newFarmer1 = accounts[1];
+    const newFarmer2 = accounts[2];
+
+    before(async()=>{
+        FarmerRoleManager = await FarmerRole.deployed();
+        console.log("\nSupplyChain Contract deployed at: "+FarmerRoleManager.address+"\n");
+    });
+
+    it('should owner is a member of farmer',async()=>{
+        let result = await FarmerRoleManager.isFarmer(owner);
+
+        assert.equal(result,true,'Error: contract owner is not the farmer');
+    });
+
+    it('should add two new farmers success',async()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        FarmerRoleManager.FarmerAdded((err,res)=>{
+            eventEmitted = true;
+        });
+
+        //add a new farmer
+        await FarmerRoleManager.addFarmer(newFarmer1,{from:owner});
+        await FarmerRoleManager.addFarmer(newFarmer2,{from:owner});
+
+        //verify the new farmer ID
+        let result1 = await FarmerRoleManager.isFarmer(newFarmer1);
+        let result2 = await FarmerRoleManager.isFarmer(newFarmer2);
+
+        assert.equal(eventEmitted,true,'Error: FarmerAdded event not emitted.');
+        assert.equal(result1,true,'Error: '+newFarmer1+' is not the farmer.');
+        assert.equal(result2,true,'Error: '+newFarmer2+' is not the farmer');
+    });
+
+    it('should renounce a farmer role success by a farmer',async ()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        FarmerRoleManager.FarmerRemoved((err,res)=>{
+            eventEmitted = true;
+        });
+
+        //renounce the farmer role
+        await FarmerRoleManager.renounceFarmer({from:newFarmer2});
+        let result = await FarmerRoleManager.isFarmer(newFarmer2);
+
+        assert.equal(result,false,'Error:'+newFarmer2+'should be removed from roles');
+        assert.equal(eventEmitted,true,'Error: FarmerRemoved event not emitted.');
+    })
+
+    it('should failed when adding a existed farmer',async()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        FarmerRoleManager.FarmerAdded((err,res)=>{
+            eventEmitted = true;
+        });
+
+        try{
+        //add a new farmer
+        await FarmerRoleManager.addFarmer(owner,{from:owner});
+        }
+        catch(err){
+            assert.ok(err);
+        }
+        //verify the new farmer ID
+        let result = await FarmerRoleManager.isFarmer(owner);
+
+        assert.equal(eventEmitted,true,'Error: FarmerAdded event not emitted.');
+        assert.equal(result,true,'Error: '+newFarmer1+' is not the farmer.');
+    });
+
+    it('should failed when renouncing a farmer role who is not a farmer',async ()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        FarmerRoleManager.FarmerRemoved((err,res)=>{
+            eventEmitted = true;
+        });
+
+        try{
+            //renounce the farmer role
+            await FarmerRoleManager.renounceFarmer({from:accounts[5]});
+        }catch(err){
+            assert.ok(err);
+        }
+        
+        let result = await FarmerRoleManager.isFarmer(accounts[5]);
+
+        assert.equal(result,false,'Error:'+accounts[5]+'should be removed from roles');
+        assert.equal(eventEmitted,true,'Error: FarmerRemoved event not emitted.');
+    })
+});
---
+++ test/TestRetailerRole.js
@@ -0,0 +1,100 @@
+let RetailerRole = artifacts.require('RetailerRole');
+
+contract('RetailerRole',(accounts)=>{
+
+    let RetailerRoleManager;
+    const owner = accounts[0]
+    const newRetailer1 = accounts[1];
+    const newRetailer2 = accounts[2];
+
+    before(async()=>{
+        RetailerRoleManager = await RetailerRole.deployed();
+        console.log("\nSupplyChain Contract deployed at: "+RetailerRoleManager.address+"\n");
+    });
+
+    it('should owner is a member of Retailer',async()=>{
+        let result = await RetailerRoleManager.isRetailer(owner);
+
+        assert.equal(result,true,'Error: contract owner is not the Retailer');
+    });
+
+    it('should add two new Retailers success',async()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        RetailerRoleManager.RetailerAdded((err,res)=>{
+            eventEmitted = true;
+        });
+
+        //add a new Retailer
+        await RetailerRoleManager.addRetailer(newRetailer1,{from:owner});
+        await RetailerRoleManager.addRetailer(newRetailer2,{from:owner});
+
+        //verify the new Retailer ID
+        let result1 = await RetailerRoleManager.isRetailer(newRetailer1);
+        let result2 = await RetailerRoleManager.isRetailer(newRetailer2);
+
+        assert.equal(eventEmitted,true,'Error: RetailerAdded event not emitted.');
+        assert.equal(result1,true,'Error: '+newRetailer1+' is not the Retailer.');
+        assert.equal(result2,true,'Error: '+newRetailer2+' is not the Retailer');
+    });
+
+    it('should renounce a Retailer role success by a Retailer',async ()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        RetailerRoleManager.RetailerRemoved((err,res)=>{
+            eventEmitted = true;
+        });
+
+        //renounce the Retailer role
+        await RetailerRoleManager.renounceRetailer({from:newRetailer2});
+        let result = await RetailerRoleManager.isRetailer(newRetailer2);
+
+        assert.equal(result,false,'Error:'+newRetailer2+'should be removed from roles');
+        assert.equal(eventEmitted,true,'Error: RetailerRemoved event not emitted.');
+    })
+
+    it('should failed when adding a existed Retailer',async()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        RetailerRoleManager.RetailerAdded((err,res)=>{
+            eventEmitted = true;
+        });
+
+        try{
+        //add a new Retailer
+        await RetailerRoleManager.addRetailer(owner,{from:owner});
+        }
+        catch(err){
+            assert.ok(err);
+        }
+        //verify the new Retailer ID
+        let result = await RetailerRoleManager.isRetailer(owner);
+
+        assert.equal(eventEmitted,true,'Error: RetailerAdded event not emitted.');
+        assert.equal(result,true,'Error: '+newRetailer1+' is not the Retailer.');
+    });
+
+    it('should failed when renouncing a Retailer role who is not a Retailer',async ()=>{
+        let eventEmitted = false;
+
+        //watch the event
+        RetailerRoleManager.RetailerRemoved((err,res)=>{
+            eventEmitted = true;
+        });
+
+        try{
+            //renounce the Retailer role
+            await RetailerRoleManager.renounceRetailer({from:accounts[5]});
+        }catch(err){
+            assert.ok(err);
+        }
+        
+        let result = await RetailerRoleManager.isRetailer(accounts[5]);
+
+        assert.equal(result,false,'Error:'+accounts[5]+'should be removed from roles');
+        assert.equal(eventEmitted,true,'Error: RetailerRemoved event not emitted.');
+    })
+});
