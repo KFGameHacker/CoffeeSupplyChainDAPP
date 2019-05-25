1f1f011334391866a74ca4b790cfde0b6f4c636c
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-05-25 14:11:38 +0800

    deploy to ropsten test net with specific role account success.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,12 +5,13 @@ var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
-const Farmer = accounts[0]
-const Distributor = accounts[1];
-const Retailer = accounts[2];
-const Consumer = accounts[3];
+module.exports = function(deployer,network,accounts) {
 
-module.exports = function(deployer,accounts) {
+  const Farmer = accounts[0];
+  const Distributor = accounts[1];
+  const Retailer = accounts[2];
+  const Consumer = accounts[3];
+
   deployer.deploy(FarmerRole,{from:Farmer});
   deployer.deploy(DistributorRole,{from:Distributor});
   deployer.deploy(RetailerRole,{from:Retailer});
