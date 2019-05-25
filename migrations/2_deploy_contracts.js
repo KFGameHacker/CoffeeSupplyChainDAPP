e6476a132c363b7bbb391ece5f92764cab10e2eb
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-05-25 16:56:05 +0800

    test contract in ropsten & remix, 3 big bugs found

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,15 +6,9 @@ var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer,network,accounts) {
-
-  const Farmer = accounts[0];
-  const Distributor = accounts[1];
-  const Retailer = accounts[2];
-  const Consumer = accounts[3];
-
-  deployer.deploy(FarmerRole,{from:Farmer});
-  deployer.deploy(DistributorRole,{from:Distributor});
-  deployer.deploy(RetailerRole,{from:Retailer});
-  deployer.deploy(ConsumerRole,{from:Consumer});
+  deployer.deploy(FarmerRole);
+  deployer.deploy(DistributorRole);
+  deployer.deploy(RetailerRole);
+  deployer.deploy(ConsumerRole);
   deployer.deploy(SupplyChain);
 };
